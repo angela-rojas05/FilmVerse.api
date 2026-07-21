6b093e3b75ef2c13eb7ea1098d9540f9a821762f
angela-rojas05 <angela.rojas@tecdesoftware.edu.mx>
2026-07-21 10:52:30 -0600

    feat(persistence): add GeneroCrudRepository and EstudioCrudRepository as an interface

---
+++ FilmVerse.api/src/test/java/com/mx/edu/tecdesoftware/FilmVerse/api/persistence/crud/EstudioCrudRepository.java
@@ -0,0 +1,8 @@
+package com.mx.edu.tecdesoftware.FilmVerse.api.persistence.crud;
+
+import com.mx.edu.tecdesoftware.FilmVerse.api.persistence.entity.Estudio;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EstudioCrudRepository extends CrudRepository<Estudio, Integer> {
+
+}
---
+++ FilmVerse.api/src/test/java/com/mx/edu/tecdesoftware/FilmVerse/api/persistence/crud/GeneroCrudRepository.java
@@ -0,0 +1,8 @@
+package com.mx.edu.tecdesoftware.FilmVerse.api.persistence.crud;
+
+import com.mx.edu.tecdesoftware.FilmVerse.api.persistence.entity.Genero;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GeneroCrudRepository extends CrudRepository<Genero, Integer> {
+
+}
