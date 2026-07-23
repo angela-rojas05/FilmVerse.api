0726ddf67f14ffec002479d4bce33481d107dbed
angela-rojas05 <angela.rojas@tecdesoftware.edu.mx>
2026-07-23 10:30:44 -0600

    feat: add EpisodioCrudRepository to persistence/crud

---
+++ FilmVerse.api/src/test/java/com/mx/edu/tecdesoftware/FilmVerse/api/persistence/crud/EpisodioCrudRepository.java
@@ -0,0 +1,7 @@
+package com.mx.edu.tecdesoftware.FilmVerse.api.persistence.crud;
+import com.mx.edu.tecdesoftware.FilmVerse.api.persistence.entity.Episodio;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EpisodioCrudRepository extends CrudRepository<Episodio, Integer> {
+
+}
